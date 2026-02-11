b2cc4ae317e5e7e76b9a0ccab2cd4b6faa1efc83
Domenico <14530059@itisrossi.vi.it>
2026-02-11 21:46:28 +0100

    1° versione

---
+++ es_1_array.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using static System.Console;
+namespace es_1_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[30];
+            int x;
+            bool b;
+            do
+            {
+                WriteLine("Quanti numeri vuoi");
+                b = int.TryParse(ReadLine(), out x);
+            } while (!b || x < 0 || x > 30);
+            for (int i = 0; i < x; i++)
+            {
+                do
+                {
+                    WriteLine("Che numero vuoi");
+                    b = int.TryParse(ReadLine(), out n[i]);
+                } while (!b);
+            }
+            b = false;
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = i + 1; j < x; j++) //i + 1 perché così non controlla la stessa cella di i
+                {
+                    if (n[i] == n[j])
+                    {
+                        b = true;
+                        break;
+                    }
+                }
+            }
+
+            if (b)
+                WriteLine("0");
+            else
+                WriteLine("1");
+
+        }
+
+    }
+}
---
+++ es_2_array.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console;
+namespace es_2_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[30];
+            int metà;
+            Random rnd = new Random();
+            for (int i = 0; i < 30; i++)
+            {
+                n[i] = rnd.Next(0, 101);
+            }
+
+            for (int i = 0; i < 30; i++)
+            {
+                if (n[i] % 2 == 0) 
+                {
+                   metà = n[i] /2;
+                    for (int j = 0; j < 30; j++)
+                    {
+                        if (n[j] == metà)
+                        {
+                            Write($"{n[i]} {metà}  ");
+                        }
+                    }
+                }
+                
+
+            }
+        }
+    }
+}
